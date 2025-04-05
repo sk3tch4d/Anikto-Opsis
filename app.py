b11ea5380f5f8a7f312a07877c7efdf21a2f1395
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-05 19:37:54 -0400

    Add files via upload
    Style Sheet Corrected

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     if request.method == "POST":
         uploaded_files = request.files.getlist("pdfs")
         if not uploaded_files:
-            return "No files uploaded", 400
+            return render_template("index.html", error="No files uploaded.")
 
         temp_paths = []
         for file in uploaded_files:
@@ -19,24 +19,14 @@ def index():
             file.save(filename)
             temp_paths.append(filename)
 
-        # Run generator
         output_file = generate_argx_and_heatmap(temp_paths)
 
-        # Return download
         if output_file:
             return send_file(output_file, as_attachment=True)
         else:
-            return "Failed to generate report", 500
+            return render_template("index.html", error="Something went wrong generating the report.")
 
-    return '''
-    <!doctype html>
-    <title>ARGX Upload</title>
-    <h1>Upload PDF Files</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=pdfs multiple>
-      <input type=submit value=Generate>
-    </form>
-    '''
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
