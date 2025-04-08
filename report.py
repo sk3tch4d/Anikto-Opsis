8e38bd4361c0a2410b75857f8770cc2273e3eac7
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-07 22:45:24 -0400

    Update report.py
    Import heatmap

--- report.py
+++ report.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 
 from argx import write_argx, make_pay_period_fn
-#from heatmap import generate_heatmap_png
+from heatmap import generate_heatmap_png
 from parser import parse_pdf
 
 get_pay_period = make_pay_period_fn(datetime(2025, 1, 13))
