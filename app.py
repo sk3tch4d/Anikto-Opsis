77d5359c0057a31b42243385396d258c3fb759c8
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-09 18:16:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,9 @@ def dbcheck():
         return {"error": str(e)}
 
 
+with app.app_context():
+    db.create_all()
+    print("✓ Tables created.")
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        print("✓ Tables created.")
     app.run(debug=True)
