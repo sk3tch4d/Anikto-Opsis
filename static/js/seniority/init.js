9b747d72766e662d8455a4f68f9e45ebc10635e6
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-19 16:56:24 -0400

    Update init.js
    Update for refined stats search

--- static/js/seniority/init.js
+++ static/js/seniority/init.js
@@ -3,13 +3,20 @@
 // Entry Point to Initialize
 // ==============================
 
+// ==============================
+// IMPORT 
+// ==============================
 import { initSenioritySearch } from "./search.js";
 import { initComparisonPanel } from "./compare.js";
-import { populateGlobalStats, populateStats } from "./stats.js";
+import { populateStats, populateGlobalStats, refineSearchFromStat } from './stats.js';
 import { populatePositionList } from "./positions.js";
 import { setupDownloadButton } from "./downloads.js";
 import { fixMobileDatalist } from "./datalist.js";
 
+
+// ==============================
+// EXPORT 
+// ==============================
 export function initializeSeniorityApp() {
   initSenioritySearch();
   initComparisonPanel();
