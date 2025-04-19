830ff0be2caf00446716897658e1c0f4679cc11c
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-19 17:04:23 -0400

    Update init.js

--- static/js/seniority/init.js
+++ static/js/seniority/init.js
@@ -8,7 +8,7 @@
 // ==============================
 import { initSenioritySearch } from "./search.js";
 import { initComparisonPanel } from "./compare.js";
-import { populateStats, populateGlobalStats, refineSearchFromStat } from './stats.js';
+import { populateStats, populateGlobalStats } from './stats.js';
 import { populatePositionList } from "./positions.js";
 import { setupDownloadButton } from "./downloads.js";
 import { fixMobileDatalist } from "./datalist.js";
