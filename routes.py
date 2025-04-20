690faee30da9ad74f1e4be3a0bb1e02adf3c12d5
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-20 14:09:29 -0400

    Update routes.py
    Modified for search_cols - exclusive

--- routes.py
+++ routes.py
@@ -67,16 +67,15 @@ def register_routes(app):
         if usl != "Any":
             df = df[df["USL"].str.lower() == usl.lower()]
     
+        search_cols = ["Num", "Old", "Description", "USL", "Bin", "Group"]
         if term:
             if term.isdigit():
-                df = df[df["Num"].astype(str).str.contains(term)]
+                df = df[df["Num"].astype(str).str.startswith(term)]
             else:
-                #df = df[df.apply(lambda row: row.astype(str).str.lower().str.contains(term).any(), axis=1)]
-                excluded = ["QTY", "UOM"]
-                search_cols = [col for col in df.columns if col not in excluded]
-                df = df[df[search_cols].apply(lambda row: row.astype(str).str.lower().str.contains(term).any(), axis=1)]
+                df = df[df[search_cols].apply(
+                    lambda row: row.astype(str).str.lower().str.contains(term).any(), axis=1
+                )]
 
-    
         df = df.sort_values(by="QTY", ascending=False).head(100)
     
         return jsonify(df[["Num", "Old", "Bin", "Description", "USL", "QTY", "UOM", "Group"]].to_dict(orient="records"))
