aeb5d3f0323d21c5069468ca94429795318fbc7e
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-20 14:47:33 -0400

    Update routes.py
    Bug

--- routes.py
+++ routes.py
@@ -69,7 +69,10 @@ def register_routes(app):
     
         if term:
             if term.isdigit():
-                df = df[df["Num", "Old"].astype(str).str.contains(term)]
+                #df = df[df["Num", "Old"].astype(str).str.contains(term)]
+                df = df[df[["Num", "Old"]].astype(str).apply(
+                    lambda row: term in row.tolist(), axis=1
+                )]
             else:
                 #df = df[df.apply(lambda row: row.astype(str).str.lower().str.contains(term).any(), axis=1)]
                 excluded = ["QTY", "UOM", "Created", "Last_Change", "ROP", "ROQ", "Cost"]
