ac68fb5127879e3d5b613785cfeef1a89bb7ad3c
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-25 11:38:11 -0400

    Update inv_init.js
    Add: Missing import: scrollPanel

--- static/js/inventory/inv_init.js
+++ static/js/inventory/inv_init.js
@@ -8,6 +8,7 @@ import { populateInventoryStats } from "./inv_stats.js";
 import { setupInventoryDownloadSearch } from "./inv_downloads.js";
 import { highlightMatch } from '../search-utils.js';
 import { withLoadingToggle } from '../loading.js';
+import { scrollPanel } from '../panels.js';
 
 // ==============================
 // GLOBAL CONST
