fba05394e03198da7033a5de90c621bf1247a99a
sk3tch4d <sk3tch4d@protonmail.com>
2025-04-27 12:52:49 -0400

    Update inv_cleaner.py
    * - Add: Sloc/SLoc as same to USL

--- inv_cleaner.py
+++ inv_cleaner.py
@@ -11,6 +11,7 @@ from datetime import datetime
 # ==============================
 
 COLUMN_RENAMES = {
+    "SLoc": "USL",
     "Sloc": "USL",
     "Mat. #": "Material",
     "Material": "Material",
