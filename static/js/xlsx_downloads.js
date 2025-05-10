c6732c88c73640629659415b89b8fdd5d2f3f43f
sk3tch4d <sk3tch4d@protonmail.com>
2025-05-10 17:52:28 -0400

    Update xlsx_downloads.js
    Add: Inv Saved Layout

--- static/js/xlsx_downloads.js
+++ static/js/xlsx_downloads.js
@@ -13,6 +13,7 @@ const COLUMN_LAYOUTS = {
   zwdiseg_search: [ "Num", "Description", "Group", "ROP", "ROQ", "USL", "Date" ],
   zwdiseg_history: [ "Timestamp", "Search Term", "Filter Used", "Matches" ],
   inventory_search: [ "Num", "Description", "USL", "ROP", "ROQ", "Group", "Date" ],
+  inventory_saved: ["Num", "Description", "Group", "USL", "ROP", "ROQ", "Cost_Center", "Date"],
   inventory_history: [ "Timestamp", "Search Term", "Filter Used", "Matches" ],
   seniority_search: [ "Years", "First Name", "Last Name", "Status", "Position" ],
   optimization_search: ["Num", "Description", "Group", "USL", "ROP", "ROQ", "Candidate", "Confidence", "Score"],
