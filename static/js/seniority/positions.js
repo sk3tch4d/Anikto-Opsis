bc4d1896cf2bc3b05cd92198f5c657165432c50c
sk3tch4d <sk3tch4d@protonmail.com>
2025-05-13 21:50:15 -0400

    Update positions.js
    Revert delta

--- static/js/seniority/positions.js
+++ static/js/seniority/positions.js
@@ -44,12 +44,15 @@ export function populatePositionList() {
       sorted.forEach(([pos, count]) => {
         const card = document.createElement("div");
         card.className = "clickable-stat";
-        
+
         card.innerHTML = `
-        <div class="panel-delta">
-          <div class="delta-item">${toTitleCase(pos)}<br>${count}</div>
-        </div>
-      `;
+          <div class="panel-delta">
+            <div class="delta-item">
+              ${toTitleCase(pos)}
+              <span>${count}</span>
+            </div>
+          </div>
+        `;
 
         card.addEventListener("click", () => {
           searchFromStat("seniority-search", pos);
