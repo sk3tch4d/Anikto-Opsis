9030eea3a1f96e2ef6779abb3ac8941a1508c314
sk3tch4d <sk3tch4d@protonmail.com>
2025-05-13 20:58:27 -0400

    Update positions.js
    Remove compare card class

--- static/js/seniority/positions.js
+++ static/js/seniority/positions.js
@@ -43,7 +43,7 @@ export function populatePositionList() {
       const sorted = Object.entries(positionMap).sort((a, b) => b[1] - a[1]);
       sorted.forEach(([pos, count]) => {
         const card = document.createElement("div");
-        card.className = "compare-card clickable-stat";
+        card.className = "clickable-stat";
 
         card.innerHTML = `
           <div class="compare-delta">
