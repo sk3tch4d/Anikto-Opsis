86013315416d7e223d12f119e978a053e1694ebf
sk3tch4d <sk3tch4d@protonmail.com>
2025-05-31 17:57:03 -0400

    Update sen_init.js
    Updates for autocomplete datalist

--- static/js/seniority/sen_init.js
+++ static/js/seniority/sen_init.js
@@ -1,21 +1,21 @@
 // ==============================
-// INIT.JS
-// Entry Point to Initialize
+// SEN_INIT.JS
 // ==============================
 
+
 // ==============================
-// IMPORT 
+// IMPORT
 // ==============================
 import { initSenioritySearch } from "./sen_search.js";
 import { initComparisonPanel } from "./sen_compare.js";
 import { populateStats, populateGlobalStats } from './sen_stats.js';
 import { populatePositionList } from "./sen_positions.js";
 import { setupSeniorityDownloadCleaned, setupSeniorityDownloadSearch } from "./sen_downloads.js";
-import { fixMobileDatalist } from "./sen_datalist.js";
+import { initCompareAutocomplete } from "./sen_datalist.js";
 import { initSeniorityFilters } from "./sen_filters.js";
 
 // ==============================
-// EXPORT 
+// INITIALIZE
 // ==============================
 export function initializeSeniorityApp() {
   initSenioritySearch();
@@ -25,5 +25,5 @@ export function initializeSeniorityApp() {
   populatePositionList();
   setupSeniorityDownloadCleaned();
   setupSeniorityDownloadSearch();
-  fixMobileDatalist();
+  initCompareAutocomplete();
 }
