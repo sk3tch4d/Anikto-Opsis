6639020649568600e96dfebf5f05b67713b9c310
sk3tch4d <sk3tch4d@protonmail.com>
2025-06-04 14:11:34 -0400

    Update info_logging.py
    Add: info_log -> General Logging Util

--- utils/info_logging.py
+++ utils/info_logging.py
@@ -2,7 +2,7 @@
 # INFO_LOGGING.PY
 # ==============================
 
-from flask import request, current_app
+from flask import current_app, request, has_request_context
 from user_agents import parse
 import logging
 
@@ -19,3 +19,18 @@ def new_session_info():
     current_app.logger.info(f"🌐 Browser: {ua.browser.family} {ua.browser.version_string}")
     current_app.logger.info(f"🌐 OS: {ua.os.family} {ua.os.version_string}")
     current_app.logger.info(f"🌐 Device: {ua.device.family}")
+
+# ==============================
+# GENERAL INFO LOGGING UTIL
+# ==============================
+def info_log(log="", df=None, title="", message="", logger=None, level=logging.INFO):
+
+    logger = logger or (current_app.logger if has_request_context() else logging.getLogger(__name__))
+    name = df.attrs.get("name", "Unnamed") if df is not None else ""
+
+    parts = [title, log, "→", name]
+    if message:
+        parts.append(f"— {message}")
+    
+    log_message = " ".join(part for part in parts if part)
+    logger.log(level, log_message)
