fe0cf3677d464214112bbe8353039dd664e96079
sk3tch4d <sk3tch4d@protonmail.com>
2025-06-11 20:57:05 -0400

    Update data_search.py
    Updates for shared usage

--- utils/data_search.py
+++ utils/data_search.py
@@ -8,15 +8,16 @@ from flask import request, jsonify
 # ==============================
 # SEARCH REQUEST
 # ==============================
-def handle_search_request(df, search_fn, default_sort="QTY"):
+def handle_search_request(df, search_fn, default_sort="QTY", filter_param="usl"):
     try:
         term = request.args.get("term", "")
-        usl = request.args.get("usl", "Any")
+        filter_value = request.args.get(filter_param, "All")
         sort = request.args.get("sort", default_sort)
         direction = request.args.get("dir", "desc")
 
-        results = search_fn(df, term, usl, sort, direction)
+        results = search_fn(df, term, filter_value, sort, direction)
 
+        # Fix NaNs to None before jsonify
         for r in results:
             for k, v in r.items():
                 if isinstance(v, float) and np.isnan(v):
