b780c7a7b39a0740705d63397077fdab8ef8878b
sk3tch4d <sk3tch4d@protonmail.com>
2025-06-19 18:44:59 -0400

    Update panels_utils.js
    Add: scrollPanelBody

--- static/js/panels/panels_utils.js
+++ static/js/panels/panels_utils.js
@@ -2,6 +2,7 @@
 // PANELS_UTILS.JS
 // ==============================
 
+import { DEBUG_MODE } from './panels_config.js';
 
 // ==============================
 // BODY SCROLL LOCK
@@ -10,4 +11,23 @@ export function setBodyLock(lock = true) {
   const value = lock ? 'hidden' : '';
   document.documentElement.style.overflow = value;
   document.body.style.overflow = value;
+}
+
+// ==============================
+// SCROLL PANEL BODY
+// ==============================
+export function scrollPanelBody(panelId = null, behavior = 'smooth') {
+  let panel;
+
+  if (panelId) {
+    panel = document.getElementById(panelId);
+  } else {
+    panel = document.querySelector('.panel.open');
+  }
+
+  const body = panel?.querySelector('.panel-body.scrollable-panel');
+  if (body) {
+    body.scrollTo({ top: 0, behavior });
+    DEBUG_MODE && console.log(`[DEBUG] Smooth scrolled ${panel?.id || '(unknown panel)'} body to top`);
+  }
 }
